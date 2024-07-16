397306b65d5ca36d9bcb612d53857123e2f07f79
Zedan Ak <zedan.ak@dci-student.org>
2024-07-16 18:55:26 +0200

    :octocat:

---
+++ ak.js
@@ -0,0 +1,17 @@
+' ZWEI  TAGE  RECHERCHE  OHNE  ERFOLG !!! '
+
+' LÖSUNG  IN  ZEHN  MINUTEN !!! '
+
+
+
+
+console.log('\n');
+console.log('                            Unsere Tabelle');
+console.log('| #      | Name                  | Group       | Role               |');
+console.log('|--------|-----------------------|-------------|--------------------|');
+console.log('| 1      | Thingy 1              | this        | Admin              |');
+console.log('| 2      | Thingy 2              | other       | Admin              |');
+console.log('| 3      | Thingy 3              | this        | Moderator          |');
+console.log('\n');
+console.log('                       💃 🕺 💃 🕺 💃 🕺 💃');
+console.log('\n');
